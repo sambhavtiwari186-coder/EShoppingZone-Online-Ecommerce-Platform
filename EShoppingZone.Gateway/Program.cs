b1a885406688cc7644aedae3e6cb9460b99bab41
sambhav <sambhavtiwari186@gmail.com>
2026-05-15 21:42:24 +0530

    Add Vercel frontend to CORS allowed origins

--- EShoppingZone.Gateway/Program.cs
+++ EShoppingZone.Gateway/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://e-shopping-zone-online-ecommerce-pl.vercel.app")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
